58354c3f43e382a3c68642fe95a4fa49847d84b2
Ashish Karki <ashishkarki@users.noreply.github.com>
2017-07-25 08:26:54 -0400

    Module2 - Creating First Component: Commit 1

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [EventsAppComponent] 
+})
+export class AppModule {
+
+}
